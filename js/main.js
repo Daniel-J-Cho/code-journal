044995466ed848ff86115419b402a0c71a067f1a
Daniel Cho <danielcho07@gmail.com>
2022-04-07 23:51:28 +0000

    Pre-populate the entry form with the clicked entry's values from the object found in the data model. #3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 /* global data */
 /* exported data */
 
+var $entryFormh1 = document.querySelector('.entry-form-h1');
+var $titleInput = document.querySelector('.title-input');
 var $photoInput = document.querySelector('.photo-input');
+var $textArea = document.querySelector('.textarea');
 var $image = document.querySelector('.image');
 var $form = document.querySelector('.form');
 var $ul = document.querySelector('.unordered-list');
@@ -40,9 +43,12 @@ $form.addEventListener('submit', function (event) {
       <img src="some image" class="image">
     </div>
     <div class="column-half">
-      <div class="innerDiv">
+      <div class="inner-div">
         <h2></h2>
-        <i></i>
+        <div class="second-inner-div">
+        <a></a>
+          <i></i>
+        </div>
       </div>
       <p></p>
     </div>
@@ -55,19 +61,27 @@ function domTree(entry) {
   var firstColDiv = document.createElement('div');
   var imgElem = document.createElement('img');
   var secondColDiv = document.createElement('div');
-  var h2Elem = document.createElement('h2');
   var innerDiv = document.createElement('div');
+  var h2Elem = document.createElement('h2');
+  var secondInnerDiv = document.createElement('div');
   var pElem = document.createElement('p');
   var editIcon = document.createElement('i');
+  var editAnchor = document.createElement('a');
   liElem.appendChild(rowDiv);
   rowDiv.appendChild(firstColDiv);
   firstColDiv.appendChild(imgElem);
   rowDiv.appendChild(secondColDiv);
   editIcon.className = 'fas fa-pen';
+  editIcon.setAttribute('data-entry-id', entry.nextEntryId);
   secondColDiv.appendChild(innerDiv);
-  innerDiv.className = 'innerDiv';
+  innerDiv.className = 'inner-div';
   innerDiv.appendChild(h2Elem);
-  innerDiv.appendChild(editIcon);
+  innerDiv.appendChild(secondInnerDiv);
+  secondInnerDiv.className = 'second-inner-div';
+  secondInnerDiv.appendChild(editAnchor);
+  editAnchor.appendChild(editIcon);
+  editAnchor.setAttribute('href', '#entry-form');
+  editAnchor.setAttribute('class', 'edit-icon-anchor');
   secondColDiv.appendChild(pElem);
   rowDiv.setAttribute('class', 'row entries');
   firstColDiv.setAttribute('class', 'column-half');
@@ -104,4 +118,29 @@ $buttonAnchor.addEventListener('click', function (event) {
   $buttonAnchor.className = 'button-anchor hidden';
   $entryForm.className = 'entry-form';
   $entries.className = 'entries hidden';
+  $entryFormh1.textContent = 'New Entry';
+  $image.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $form.reset();
+});
+
+$ul.addEventListener('click', function (event) {
+  if (event.target.matches('i')) {
+    $entryForm.className = 'entry-form';
+    $entries.className = 'entries hidden';
+    var nextEntryIdString = event.target.getAttribute('data-entry-id');
+    var nextEntryIdNum = parseInt(nextEntryIdString, 10);
+    for (var j = 0; j < data.entries.length; j++) {
+      if (nextEntryIdNum === data.entries[j].nextEntryId) {
+        data.editing = data.entries[j];
+      }
+    }
+    $entryFormh1.textContent = 'Edit Entry';
+    $titleInput.value = data.editing.title;
+    $photoInput.value = data.editing.photo;
+    $image.setAttribute('src', data.editing.photo);
+    $textArea.value = data.editing.notes;
+  } else {
+    $entries.className = 'entries';
+    $entryForm.className = 'entry-form hidden';
+  }
 });
